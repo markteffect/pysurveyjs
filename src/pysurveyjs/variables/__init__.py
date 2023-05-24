e9fef21193bef53570ba9e9d16b0e55449a99d6d
Rick Wouters <rick@happytechnology.nl>
2023-05-24 13:34:44 +0200

    Add variables to variables, bump version

--- src/pysurveyjs/variables/__init__.py
+++ src/pysurveyjs/variables/__init__.py
@@ -2,3 +2,5 @@ from pysurveyjs.variables.variable import Variable
 from pysurveyjs.variables.boolean_variable import BooleanVariable
 from pysurveyjs.variables.numeric_variable import NumericVariable
 from pysurveyjs.variables.string_variable import StringVariable
+from pysurveyjs.variables.multiple_choice_variable import MultipleChoiceVariable
+from pysurveyjs.variables.single_choice_variable import SingleChoiceVariable
